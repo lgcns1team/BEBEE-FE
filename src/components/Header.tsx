bf38536371396817c720bbf294d55638621a4c93
dohee <rhehgml00@naver.com>
2025-12-05 10:26:55 +0900

    style:헤더 스타일 통일, 버튼 스타일 통일

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,59 +1,118 @@
+/*뒤로가기 아이콘은 무조건 있음
+더보기 ? showRight : 없음
+onRightClick ? 더보기 클릭시 실행할 함수 : 없음
+
+# 사용 예시
+<Header
+  title="내 프로필"
+  onBack={() => navigate(-1)}
+  showRight
+  onRightClick={() => setOpen(true)}
+/>
+*/
+
 import styled from "styled-components";
 import { IoChevronBack } from "react-icons/io5";
+import { FiMoreVertical } from "react-icons/fi";
 
 interface HeaderProps {
-  title: string;
-  onBack?: () => void;
-  rightElement?: React.ReactNode;
+  title?: string;
+  subTitle?: string;
+  onBack: () => void;
+  onRightClick?: () => void;
+  showRight?: boolean;
 }
 
-const Header = ({ title, onBack, rightElement }: HeaderProps) => {
+const Header = ({
+  title,
+  subTitle,
+  onBack,
+  onRightClick,
+  showRight,
+}: HeaderProps) => {
   return (
     <Container>
+      {/* 왼쪽: 항상 노출 */}
       <Left onClick={onBack}>
         <IoChevronBack size={25} />
       </Left>
 
-      <Title>{title}</Title>
+      {/* 타이틀 영역 */}
+      {(title || subTitle) && (
+        <HeaderTitleBox>
+          {title && <Title>{title}</Title>}
+          {subTitle && <SubTitle>{subTitle}</SubTitle>}
+        </HeaderTitleBox>
+      )}
 
-      <Right>{rightElement}</Right>
+      {/* 더보기 필요할 때만*/}
+      {showRight && (
+        <Right onClick={onRightClick}>
+          <FiMoreVertical size={25} />
+        </Right>
+      )}
     </Container>
   );
 };
 
 export default Header;
 
+// ---------- styled ----------
 const Container = styled.header`
   width: 100%;
   height: 73px;
   display: flex;
   align-items: center;
-  box-sizing: border-box;
   position: relative;
+  box-sizing: border-box;
   margin-bottom: 20px;
 `;
 
 const Left = styled.div`
-  width: 30px;
+  position: absolute;
+  left: 0;
   height: 30px;
+  width: 30px;
   display: flex;
   align-items: center;
   cursor: pointer;
   color: ${({ theme }) => theme.color.text};
 `;
 
-const Title = styled.h1`
-  flex: 1;
-  text-align: center;
-  font-size: ${({ theme }) => theme.size.lg};
-  font-weight: 600;
-  color: #1c1c1c;
-`;
-
 const Right = styled.div`
-  width: 30px;
+  position: absolute;
+  right: 0;
   height: 30px;
+  width: 30px;
   display: flex;
   justify-content: flex-end;
   align-items: center;
+  cursor: pointer;
+`;
+
+const HeaderTitleBox = styled.div`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  white-space: nowrap;
+`;
+
+const Title = styled.h1`
+  font-size: ${({ theme }) => theme.size.lg};
+  font-weight: ${({ theme }) => theme.weight.bold};
+  color: ${({ theme }) => theme.color.text};
+  margin: 0;
+`;
+
+const SubTitle = styled.div`
+  font-size: ${({ theme }) => theme.size.sm};
+  background-color: ${({ theme }) => theme.color.subColor2};
+  color: ${({ theme }) => theme.color.main};
+  padding: 2px 6px;
+  border-radius: ${({ theme }) => theme.borderRadius.lg};
+  border: 0.5px solid ${({ theme }) => theme.color.main};
+  margin: 0;
 `;
