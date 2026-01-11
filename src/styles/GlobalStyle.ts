9136b03f7b2c56181b8bafb01c62b169efe85cec
dohee <rhehgml00@naver.com>
2026-01-11 14:17:06 +0900

    sytle:랜딩페이지 디자인 변경&home에서 뒤로가기 방지

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -8,32 +8,44 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  body {
+  html, body {
+
+    background-color: ${theme.color.white}; 
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  #root {
     width: 375px;
     max-width: 100%;
     margin: 0 auto;
+    min-height: 100vh; 
+    
+ background-color: transparent;
+    color: ${theme.color.text};
+    
     font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
     font-size: ${theme.size.md};
-    color: ${theme.color.text};
-    background-color: ${theme.color.white};
     line-height: 1.4;
- 
+    
   }
 
   a {
     text-decoration: none;
     color: inherit;
   }
-.sr-only {
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  padding: 0;
-  margin: -1px;
-  overflow: hidden;
-  clip: rect(0, 0, 0, 0);
-  white-space: nowrap;
-  border-width: 0;
-}
 
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border-width: 0;
+  }
 `;
