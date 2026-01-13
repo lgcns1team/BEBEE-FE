d22a4f87bd781524da9f77842d6e293738a21342
dohee <rhehgml00@naver.com>
2026-01-13 19:31:27 +0900

    feat: PWA와 FCM 서비스 워커 충돌 방지 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,21 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      manifestFilename: 'manifest.json',
-      manifest: false
-    })
+      injectRegister: null,
+      registerType: "autoUpdate",
+      // Firebase 서비스 워커를 제외하도록 설정하거나,
+      // 직접 만든 sw.js를 PWA 워커로 사용하려면 모드 변경이 필요합니다.
+      workbox: {
+        // public에 있는 firebase-messaging-sw.js를
+        // PWA의 캐싱 대상에서 제외하여 충돌 방지
+        globIgnores: ["firebase-messaging-sw.js"],
+      },
+      manifest: false,
+    }),
   ],
   resolve: {
     alias: {
