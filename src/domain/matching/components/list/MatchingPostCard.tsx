65b155e603a159f153af987bba1710ca03070ec7
minsunmanju <157439430+minsunmanju@users.noreply.github.com>
2026-01-15 17:56:57 +0900

    MatchingPostCard.tsx margin 추가

--- src/domain/matching/components/list/MatchingPostCard.tsx
+++ src/domain/matching/components/list/MatchingPostCard.tsx
@@ -261,7 +261,7 @@ const Thumbnail = styled.div`
   height: 80px;
   border-radius: ${({ theme }) => theme.borderRadius.sm};
   overflow: hidden;
-
+margin-top: 20px;
   img {
     width: 100%;
     height: 100%;
