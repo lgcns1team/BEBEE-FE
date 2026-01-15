9d1cd003378d3eff42476493da3deea9f34852e3
minsunmanju <157439430+minsunmanju@users.noreply.github.com>
2026-01-15 21:15:31 +0900

    MatchingPostCard.tsx 스크린 리더용 수정

--- src/domain/matching/components/list/MatchingPostCard.tsx
+++ src/domain/matching/components/list/MatchingPostCard.tsx
@@ -102,7 +102,7 @@ const MatchingPostCard = ({ engagement, onComplete }: Props) => {
   };
 
   return (
-    <Card role="button" aria-label={cardAriaLabel} tabIndex={0} >
+    <Card aria-label={cardAriaLabel} tabIndex={0} >
       <div aria-hidden="true">
         <TopArea>
           <Title
